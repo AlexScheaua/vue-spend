e64789a05b95df3e0f130aee77f937082be70c59
Alex Scheaua <alexandru-florin.scheaua@stefanini.com>
2019-08-09 10:47:15 +0300

    Beta 1.1.0 - login moved to backend

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-let express = require('express');
-// import path from 'path';
-let serveStatic = require('serve-static');
-let history = require('connect-history-api-fallback');
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const history = require('connect-history-api-fallback');
 
-let app = express();
+const app = express();
+app.use(express.json());
+app.use(cors());
 app.use(history());
-app.use(serveStatic(__dirname + "/dist"));
+app.use(express.static('dist'));
+
+app.put('/auth', async (req, res) => {
+  let response = '';
+
+  await fetch(`https://vspend.firebaseio.com/users/${req.body.name}.json`)
+    .then(resp => resp.json())
+    .then(data => {
+      if (req.body.collection === data.collection) {
+        response = data.link;
+      } else {
+        response = false;
+      }
+    })
+  res.status(200).send(JSON.stringify(response));
+
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log('server started ' + port);
